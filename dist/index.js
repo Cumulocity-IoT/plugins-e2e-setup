1f6ffd80912cd8391a7cacbde976c3c7479e2a45
jdre <jdre@softwareag.com>
2024-07-04 09:45:19 +0200

    Add collect shell versions script

--- dist/index.js
+++ dist/index.js
@@ -29188,6 +29188,98 @@ function wrappy (fn, cb) {
 
 /***/ }),
 
+/***/ 8351:
+/***/ ((__unused_webpack_module, exports, __nccwpck_require__) => {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.collectShellVersions = void 0;
+const core = __nccwpck_require__(2186);
+const { exec } = __nccwpck_require__(2081);
+const util = __nccwpck_require__(3837);
+// const fs = require('fs');
+const execPromise = util.promisify(exec);
+/**
+ * Fetches the distribution tags for a given npm package.
+ * @param {string} packageName - The name of the npm package.
+ * @returns {Promise<Record<DistTag, Version>>} A promise that resolves to an object containing the distribution tags.
+ * @throws Will throw an error if the execution of the npm view command fails.
+ */
+async function getDistTagsObject(packageName) {
+    try {
+        const { stdout } = await execPromise(`npm view ${packageName} dist-tags --json`);
+        return JSON.parse(stdout);
+    }
+    catch (error) {
+        console.error('Error fetching dist-tags:', error);
+        return {};
+    }
+}
+/**
+ * Checks if a specific version of a given npm package is deprecated.
+ * @param {string} packageName - The name of the npm package.
+ * @param {string} version - The version of the npm package.
+ * @returns {Promise<boolean>} A promise that resolves to a boolean indicating whether the version is deprecated.
+ * @throws Will throw an error if the execution of the npm view command fails.
+ */
+async function isDeprecated(packageName, version) {
+    try {
+        const deprecatedInfo = (await execPromise(`npm view ${packageName}@${version} deprecated --json`))?.stdout;
+        return !!deprecatedInfo;
+    }
+    catch (error) {
+        console.error(`Error checking if ${packageName}@${version} is deprecated:`, error);
+        return false;
+    }
+}
+/**
+ * Fetches the non-deprecated versions of a given npm package.
+ * @param {string} packageName - The name of the npm package.
+ * @returns {Promise<{tag: string, version: string, major: string}[]>} A promise that resolves to an array containing the non-deprecated versions.
+ */
+async function getLastNonDeprecatedVersions(packageName) {
+    const distTagsObject = await getDistTagsObject(packageName);
+    const nonDeprecatedVersionsObject = {};
+    for (const [tag, version] of Object.entries(distTagsObject)) {
+        const deprecated = await isDeprecated(packageName, version);
+        if (!deprecated) {
+            nonDeprecatedVersionsObject[tag] = version;
+        }
+    }
+    const yearlyReleasePattern = /^y\d{4}-lts$/;
+    let yearlyReleaseVersions = Object.entries(distTagsObject)
+        .filter(([key, _]) => yearlyReleasePattern.test(key))
+        .slice(0, 3);
+    if (yearlyReleaseVersions.length < 3 && distTagsObject['1018.0-lts']) {
+        yearlyReleaseVersions.push(['1018.0-lts', distTagsObject['1018.0-lts']]);
+    }
+    return yearlyReleaseVersions.map(([tag, version]) => ({
+        tag,
+        version,
+        major: version.split('.')[0],
+    }));
+}
+/**
+ * Returns list of last three, non-deprecated versions of @c8y/ngx-components package.
+ */
+const collectShellVersions = async () => {
+    const packageName = '@c8y/ngx-components';
+    const nonDeprecatedVersions = await getLastNonDeprecatedVersions(packageName);
+    core.setOutput("non_deprecated_shell_versions", JSON.stringify(nonDeprecatedVersions));
+    // const outputPath = process.env.GITHUB_OUTPUT;
+    // if (outputPath) {
+    //   fs.appendFileSync(
+    //     outputPath,
+    //     `non_deprecated_shell_versions=${JSON.stringify(nonDeprecatedVersions)}\n`
+    //   );
+    // }
+};
+exports.collectShellVersions = collectShellVersions;
+
+
+/***/ }),
+
 /***/ 4978:
 /***/ ((module) => {
 
@@ -29217,6 +29309,14 @@ module.exports = require("async_hooks");
 
 "use strict";
 module.exports = require("buffer");
+
+/***/ }),
+
+/***/ 2081:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("child_process");
 
 /***/ }),
 
@@ -31079,22 +31179,27 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be in strict mode.
 (() => {
 "use strict";
+var exports = __webpack_exports__;
 
-const core = __nccwpck_require__(2186);
+Object.defineProperty(exports, "__esModule", ({ value: true }));
 const github = __nccwpck_require__(5438);
-try {
+const collect_shell_versions_1 = __nccwpck_require__(8351);
+const core_1 = __nccwpck_require__(2186);
+const performAction = async () => {
     // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
+    const nameToGreet = (0, core_1.getInput)("who-to-greet");
     console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+    const time = new Date().toTimeString();
+    (0, core_1.setOutput)("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
-}
-catch (error) {
-    core.setFailed(error.message);
-}
+    await (0, collect_shell_versions_1.collectShellVersions)();
+};
+performAction().catch((error) => {
+    console.error("An error occurred", error);
+    (0, core_1.setFailed)(error.message);
+});
 
 })();
 
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const github = require("@actions/github");
+import { collectShellVersions } from "./collect-shell-versions";
+import { setFailed, setOutput, getInput } from "@actions/core";
 
-try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+const performAction = async () => {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = getInput("who-to-greet");
+    console.log(`Hello ${nameToGreet}!`);
+    const time = new Date().toTimeString();
+    setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
+    console.log(`The event payload: ${payload}`);
+
+    await collectShellVersions();
+
+};
+
+performAction().catch((error) => {
+  console.error("An error occurred", error);
+  setFailed(error.message);
+});
