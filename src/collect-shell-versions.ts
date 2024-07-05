6e2a4645160224cd62061c17dbc9625ef345fffd
jdre <jdre@softwareag.com>
2024-07-05 10:15:06 +0200

    Linting fixes.

--- src/collect-shell-versions.ts
+++ src/collect-shell-versions.ts
@@ -1,8 +1,7 @@
-import { setFailed } from "@actions/core";
-const core = require("@actions/core");
-const { exec } = require("child_process");
-const util = require("util");
-// const fs = require('fs');
+import { setFailed } from '@actions/core';
+import * as core from '@actions/core';
+import { exec } from 'child_process';
+import * as util from 'util';
 const execPromise = util.promisify(exec);
 
 /**
@@ -12,17 +11,17 @@ const execPromise = util.promisify(exec);
  * @throws Will throw an error if the execution of the npm view command fails.
  */
 async function getDistTagsObject(
-  packageName: string
+	packageName: string
 ): Promise<Record<string, string>> {
-  try {
-    const { stdout } = await execPromise(
-      `npm view ${packageName} dist-tags --json`
-    );
-    return JSON.parse(stdout);
-  } catch (error) {
-    console.error("Error fetching dist-tags:", error);
-    return {};
-  }
+	try {
+		const { stdout } = await execPromise(
+			`npm view ${packageName} dist-tags --json`
+		);
+		return JSON.parse(stdout);
+	} catch (error) {
+		console.error('Error fetching dist-tags:', error);
+		return {};
+	}
 }
 
 /**
@@ -33,18 +32,18 @@ async function getDistTagsObject(
  * @throws Will throw an error if the execution of the npm view command fails.
  */
 async function isDeprecated(packageName: string, version: string) {
-  try {
-    const deprecatedInfo = (
-      await execPromise(`npm view ${packageName}@${version} deprecated --json`)
-    )?.stdout;
-    return !!deprecatedInfo;
-  } catch (error) {
-    console.error(
-      `Error checking if ${packageName}@${version} is deprecated:`,
-      error
-    );
-    return false;
-  }
+	try {
+		const deprecatedInfo = (
+			await execPromise(`npm view ${packageName}@${version} deprecated --json`)
+		)?.stdout;
+		return !!deprecatedInfo;
+	} catch (error) {
+		console.error(
+			`Error checking if ${packageName}@${version} is deprecated:`,
+			error
+		);
+		return false;
+	}
 }
 
 /**
@@ -53,48 +52,48 @@ async function isDeprecated(packageName: string, version: string) {
  * @returns {Promise<{tag: string, version: string, major: string}[]>} A promise that resolves to an array containing the non-deprecated versions.
  */
 async function getLastNonDeprecatedVersions(packageName: string) {
-  const distTagsObject = await getDistTagsObject(packageName);
-  const nonDeprecatedVersionsObject: Record<string, string> = {};
+	const distTagsObject = await getDistTagsObject(packageName);
+	const nonDeprecatedVersionsObject: Record<string, string> = {};
 
-  for (const [tag, version] of Object.entries(distTagsObject)) {
-    const deprecated = await isDeprecated(packageName, version);
-    if (!deprecated) {
-      nonDeprecatedVersionsObject[tag] = version;
-    }
-  }
-  const yearlyReleasePattern = /^y\d{4}-lts$/;
-  let yearlyReleaseVersions = Object.entries(distTagsObject)
-    .filter(([key, _]) => yearlyReleasePattern.test(key))
-    .slice(0, 3);
-  if (yearlyReleaseVersions.length < 3 && distTagsObject["1018.0-lts"]) {
-    yearlyReleaseVersions.push(["1018.0-lts", distTagsObject["1018.0-lts"]]);
-  }
+	for (const [tag, version] of Object.entries(distTagsObject)) {
+		const deprecated = await isDeprecated(packageName, version);
+		if (!deprecated) {
+			nonDeprecatedVersionsObject[tag] = version;
+		}
+	}
+	const yearlyReleasePattern = /^y\d{4}-lts$/;
+	const yearlyReleaseVersions = Object.entries(distTagsObject)
+		.filter(([key, _]) => yearlyReleasePattern.test(key))
+		.slice(0, 3);
+	if (yearlyReleaseVersions.length < 3 && distTagsObject['1018.0-lts']) {
+		yearlyReleaseVersions.push(['1018.0-lts', distTagsObject['1018.0-lts']]);
+	}
 
-  return yearlyReleaseVersions.map(([tag, version]) => ({
-    tag,
-    version,
-    major: version.split(".")[0],
-  }));
+	return yearlyReleaseVersions.map(([tag, version]) => ({
+		tag,
+		version,
+		major: version.split('.')[0]
+	}));
 }
 
 /**
  * Returns list of last three, non-deprecated versions of @c8y/ngx-components package.
  */
 const collectShellVersions = async () => {
-  const packageName = "@c8y/ngx-components";
-  const nonDeprecatedVersions = await getLastNonDeprecatedVersions(packageName);
+	const packageName = '@c8y/ngx-components';
+	const nonDeprecatedVersions = await getLastNonDeprecatedVersions(packageName);
 
-  core.setOutput(
-    "non_deprecated_shell_versions",
-    JSON.stringify(nonDeprecatedVersions)
-  );
+	core.setOutput(
+		'non_deprecated_shell_versions',
+		JSON.stringify(nonDeprecatedVersions)
+	);
 };
 
 const performAction = async () => {
-  await collectShellVersions();
+	await collectShellVersions();
 };
 
-performAction().catch((error) => {
-  console.error("An error occurred", error);
-  setFailed(error.message);
+performAction().catch(error => {
+	console.error('An error occurred', error);
+	setFailed(error.message);
 });
